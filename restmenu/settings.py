a9aae034e2334450c397f2e706f990ab4628aa93
petr <cechpetr88@gmail.com>
2021-03-13 09:14:37 +0100

    Requirmenst edited

--- restmenu/settings.py
+++ restmenu/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ['SECRET_KEY']
+SECRET_KEY = 'wz1gti$ophgy5^ieuqy(+ry(6xx6^)-r41ghez^_+px!0!_c6h'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
@@ -125,6 +125,8 @@ STATIC_URL = '/static/'
 
 MEDIA_URL = '/images/'
 
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static')
-    ]
+#STATICFILES_DIRS = [
+    #os.path.join(BASE_DIR, "static")
+    #]
+
+STATIC_ROOT = ("static")
